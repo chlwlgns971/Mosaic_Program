bbd7778c809e95af68d2ed47018d89b9cfa8b4d9
Jihoon <chlwlgns971@gmail.com>
2021-12-23 17:43:16 +0900

    초기 커밋.

---
+++ OpenCV-Project/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿
+namespace OpenCV_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileOpenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(735, 418);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileOpenToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1067, 28);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileOpenToolStripMenuItem
+            // 
+            this.fileOpenToolStripMenuItem.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.fileOpenToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileOpenToolStripMenuItem.Name = "fileOpenToolStripMenuItem";
+            this.fileOpenToolStripMenuItem.Size = new System.Drawing.Size(63, 24);
+            this.fileOpenToolStripMenuItem.Text = "File(&F)";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(130, 26);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(130, 26);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(920, 289);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 63);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Face AI Mosaic";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.trackBar2);
+            this.groupBox1.Controls.Add(this.trackBar1);
+            this.groupBox1.Font = new System.Drawing.Font("한컴 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.groupBox1.Location = new System.Drawing.Point(753, 31);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(302, 223);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "모자이크 설정";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("한컴 고딕", 8F, System.Drawing.FontStyle.Bold);
+            this.label6.Location = new System.Drawing.Point(256, 190);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 18);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "large";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("한컴 고딕", 8F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(256, 94);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 18);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "large";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("한컴 고딕", 8F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(15, 190);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 18);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "small";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("한컴 고딕", 8F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(13, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 18);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "small";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("한컴 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(96, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 22);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "모자이크 범위";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("한컴 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(96, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 22);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "모자이크 크기";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(6, 161);
+            this.trackBar2.Maximum = 100;
+            this.trackBar2.Minimum = 10;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(290, 56);
+            this.trackBar2.TabIndex = 7;
+            this.trackBar2.Value = 10;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(6, 64);
+            this.trackBar1.Maximum = 30;
+            this.trackBar1.Minimum = 10;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(290, 56);
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.Value = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(771, 289);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 63);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Full Mosaic";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(771, 378);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(278, 44);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 471);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Mosaic Picture";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileOpenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ OpenCV-Project/Form1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+using System.Runtime.InteropServices;
+using Point = System.Drawing.Point;
+
+namespace OpenCV_Project
+{
+    public partial class Form1 : Form
+    {
+        private Point p;
+        Image mOriginal;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                try
+                {
+                    Image img = pictureBox1.Image.Clone() as Image;
+                    Bitmap map = new Bitmap(img);
+                    
+                    //마우스가 가리키는 좌표와 실제 사진의 좌표가 일치하지 않아 일치하도록 좌표를 변경시켜 줌
+                    p.X = map.Width * e.X / pictureBox1.Width;
+                    p.Y = map.Height * e.Y / pictureBox1.Height;
+                    pictureBox1.Image = mosaicImage(map, trackBar1.Value, p.X, p.Y,trackBar2.Value); //(모자이크 할 이미지, 모자이크 크기, X좌표, Y좌표, 모자이크 범위)
+                }
+                catch
+                {
+                    MessageBox.Show("Out of range!","Error");
+                }
+
+            }
+
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                //마우스 클릭 위치 저장
+                //p.X = e.X;
+                //p.Y = e.Y;
+                Bitmap b = new Bitmap(pictureBox1.Image);
+                //마우스가 가리키는 좌표와 실제 사진의 좌표가 일치하지 않아 일치하도록 좌표를 변경시켜 줌
+                p.X = b.Width * e.X / pictureBox1.Width;
+                p.Y = b.Height * e.Y / pictureBox1.Height;
+                pictureBox1.Image = mosaicImage( b, trackBar1.Value, p.X, p.Y, trackBar2.Value); //(모자이크 할 이미지, 모자이크 크기, X좌표, Y좌표, 모자이크 범위)
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string file_path = null;
+            openFileDialog1.Title = "Load Directory";
+            openFileDialog1.InitialDirectory = "C:\\";//파일 여는 위치설정
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                file_path = openFileDialog1.FileName; //선택한 파일의 풀 경로를 불러와 저장
+                pictureBox1.Image = new Bitmap(file_path);
+                mOriginal = pictureBox1.Image.Clone() as Image;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) 
+        {
+            String filenameFaceCascade = "C:\\Users\\wkdeh\\Desktop\\graduateWorkData\\haarcascade_frontalface_alt.xml";
+            CascadeClassifier faceCascade = new CascadeClassifier();
+            Bitmap b = new Bitmap(pictureBox1.Image);
+
+            if (!faceCascade.Load(filenameFaceCascade))
+            {
+                Console.WriteLine("error");
+                return;
+            }
+
+            // detect 
+            Mat mat = OpenCvSharp.Extensions.BitmapConverter.ToMat(b);
+            Rect[] faces = faceCascade.DetectMultiScale(mat);
+            foreach (var item in faces)
+            {
+                facemosaicImage(b, trackBar1.Value, item.X, item.Y, item.Width, item.Height);
+            }
+            pictureBox1.Image = b;
+        }
+        public static Image fullmosaicImage(System.Drawing.Bitmap bitmap, int effectWidth)
+        {
+            for (int heightOfffset = 0; heightOfffset < bitmap.Height; heightOfffset += effectWidth)
+            {
+                for (int widthOffset = 0; widthOffset < bitmap.Width; widthOffset += effectWidth)
+                {
+                    int avgR = 0, avgG = 0, avgB = 0;
+                    int blurPixelCount = 0;
+
+                    for (int i = widthOffset; (i < widthOffset + effectWidth && i < bitmap.Width); i++)
+                    {
+                        for (int j = heightOfffset; (j < heightOfffset + effectWidth && j < bitmap.Height); j++)
+                        {
+                            System.Drawing.Color pixel = bitmap.GetPixel(i, j);
+
+                            avgR += pixel.R;
+                            avgG += pixel.G;
+                            avgB += pixel.B;
+
+                            blurPixelCount++;
+                        }
+                    }
+
+                    // 픽셀 RGB값의 평균값 구하기
+                    avgR = avgR / blurPixelCount;
+                    avgG = avgG / blurPixelCount;
+                    avgB = avgB / blurPixelCount;
+
+
+                    //  위에서 구한 RGB 평균값을 범위 내 모든 픽셀에 적용
+                    for (int i = widthOffset; (i < widthOffset + effectWidth && i < bitmap.Width); i++)
+                    {
+                        for (int j = heightOfffset; (j < heightOfffset + effectWidth && j < bitmap.Height); j++)
+                        {
+
+                            System.Drawing.Color newColor = System.Drawing.Color.FromArgb(avgR, avgG, avgB);
+                            bitmap.SetPixel(i, j, newColor);
+                        }
+                    }
+                }
+            }
+            return bitmap;
+        }
+        public static Image mosaicImage(System.Drawing.Bitmap bitmap, int effectWidth, int x, int y, int value)
+        {
+            for (int heightOfffset = y; (heightOfffset < y+value && heightOfffset<bitmap.Height); heightOfffset += effectWidth)
+            {
+                for (int widthOffset = x; (widthOffset < x+value && widthOffset<bitmap.Width); widthOffset += effectWidth)
+                {
+                    int avgR = 0, avgG = 0, avgB = 0;
+                    int blurPixelCount = 0;
+
+                    for (int i = widthOffset; (i < widthOffset + effectWidth && i < x+value && i<bitmap.Width); i++)
+                    {
+                        for (int j = heightOfffset; (j < heightOfffset + effectWidth && j < y+value && j<bitmap.Height); j++)
+                        {
+                            System.Drawing.Color pixel = bitmap.GetPixel(i, j);
+
+                            avgR += pixel.R;
+                            avgG += pixel.G;
+                            avgB += pixel.B;
+
+                            blurPixelCount++;
+                        }
+                    }
+
+                    // 픽셀 RGB값의 평균값 구하기
+                    avgR = avgR / blurPixelCount;
+                    avgG = avgG / blurPixelCount;
+                    avgB = avgB / blurPixelCount;
+
+
+                    //  위에서 구한 RGB 평균값을 범위 내 모든 픽셀에 적용
+                    for (int i = widthOffset; (i < widthOffset + effectWidth && i < x+value && i<bitmap.Width); i++)
+                    {
+                        for (int j = heightOfffset; (j < heightOfffset + effectWidth && j < y+value && j<bitmap.Height); j++)
+                        {
+                            System.Drawing.Color newColor = System.Drawing.Color.FromArgb(avgR, avgG, avgB);
+                            bitmap.SetPixel(i, j, newColor);
+                        }
+                    }
+                }
+            }
+            return bitmap;
+        }
+        public static Image facemosaicImage(System.Drawing.Bitmap bitmap, int effectWidth, int x, int y, int width, int height)
+        {
+            for (int heightOfffset = y; (heightOfffset < y + height && heightOfffset < bitmap.Height); heightOfffset += effectWidth)
+            {
+                for (int widthOffset = x; (widthOffset < x + width && widthOffset < bitmap.Width); widthOffset += effectWidth)
+                {
+                    int avgR = 0, avgG = 0, avgB = 0;
+                    int blurPixelCount = 0;
+
+                    for (int i = widthOffset; (i < widthOffset + effectWidth && i < x + width && i < bitmap.Width); i++)
+                    {
+                        for (int j = heightOfffset; (j < heightOfffset + effectWidth && j < y + height && j < bitmap.Height); j++)
+                        {
+                            System.Drawing.Color pixel = bitmap.GetPixel(i, j);
+
+                            avgR += pixel.R;
+                            avgG += pixel.G;
+                            avgB += pixel.B;
+
+                            blurPixelCount++;
+                        }
+                    }
+
+                    // 픽셀 RGB값의 평균값 구하기
+                    avgR = avgR / blurPixelCount;
+                    avgG = avgG / blurPixelCount;
+                    avgB = avgB / blurPixelCount;
+
+
+                    //  위에서 구한 RGB 평균값을 범위 내 모든 픽셀에 적용
+                    for (int i = widthOffset; (i < widthOffset + effectWidth && i < x + width && i < bitmap.Width); i++)
+                    {
+                        for (int j = heightOfffset; (j < heightOfffset + effectWidth && j < y + height && j < bitmap.Height); j++)
+                        {
+                            System.Drawing.Color newColor = System.Drawing.Color.FromArgb(avgR, avgG, avgB);
+                            bitmap.SetPixel(i, j, newColor);
+                        }
+                    }
+                }
+            }
+            return bitmap;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null) {
+                Image img = pictureBox1.Image.Clone() as Image;
+                Bitmap map = new Bitmap(img);
+                pictureBox1.Image = fullmosaicImage(map, trackBar1.Value);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                pictureBox1.Image = mOriginal;
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //    //이미지 저장경로 지정
+            //    string saveFolder = @"C:\\Users\\wkdeh\\Desktop\\강의자료\\2021년 2학기\\오픈소스개발프로젝트\\OpenCV-Project";
+
+            //    //이미지 저장경로 확인 및 생성
+            //    if (!System.IO.Directory.Exists(saveFolder))
+            //        System.IO.Directory.CreateDirectory(saveFolder);
+
+            //    //jpg 저장
+            //    pictureBox1.Image.Save(saveFolder + "image.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+            string save_path = null;
+            saveFileDialog1.Title = "Save Directory";
+            saveFileDialog1.OverwritePrompt = true;
+            saveFileDialog1.Filter = "JPEG File(*.jpg)|*.jpg|Bitmap File(*.bmp)|*.bmp|PNG FIle(*.png)|*.png";
+            saveFileDialog1.InitialDirectory = "C:\\";//파일 여는 위치설정
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                save_path = saveFileDialog1.FileName;
+                pictureBox1.Image.Save(save_path);
+            }
+        }
+    }
+}
